6ceeb61db60c8e1dce44cb894f7707eb97c32e6f
root <nagaraja.hm.0001@gmail.com>
2021-12-14 17:11:25 +0100

    Update: submodule Swc_EcuM

--- LATEST/inc/Swc_EcuM.h
+++ LATEST/inc/Swc_EcuM.h
@@ -2,8 +2,14 @@
 
 class class_Swc_EcuM{
    public:
-      void StartPreOs  (void);
-      void StartPostOs (void);
+      void StartPreOs             (void);
+      void StartPostOs            (void);
+      void OffPreOs               (void);
+      void OffPostOs              (void);
+      void EnableWakeupSources    (void);
+      void GenerateRamHash        (void);
+      void CheckRamHash           (void);
+      void ErrorHook              (void);
 };
 
 extern class_Swc_EcuM Swc_EcuM;
--- LATEST/src/Swc_EcuM.cpp
+++ LATEST/src/Swc_EcuM.cpp
@@ -1,6 +1,8 @@
 #include "EcuM.h"
 
 #include "Swc_EcuM.h"
+#include "SchM.h"
+#include "BswM.h"
 
 class_Swc_EcuM Swc_EcuM;
 
@@ -8,9 +10,50 @@ static void SetProgrammableInterrupts(void){
 }
 
 static void DriverInitZero(void){
+/*
+Adc.InitFunction(configuration abstract type);
+BswM.InitFunction(configuration abstract type);
+Can.InitFunction(configuration abstract type);
+CanTrcv.InitFunction(configuration abstract type);
+Com.InitFunction(configuration abstract type);
+Nm.InitFunction(configuration abstract type);
+Dem.InitFunction(configuration abstract type);
+Det.InitFunction(configuration abstract type);
+Eth.InitFunction(configuration abstract type);
+EthSwt.InitFunction(configuration abstract type);
+EthTrcv.InitFunction(configuration abstract type);
+Fls.InitFunction(configuration abstract type);
+Fr.InitFunction(configuration abstract type);
+FrTrcv.InitFunction(configuration abstract type);
+Gpt.InitFunction(configuration abstract type);
+Icu.InitFunction(configuration abstract type);
+IoAbHw.InitFunction(configuration abstract type);
+Lin.InitFunction(configuration abstract type);
+LinTrcv.InitFunction(configuration abstract type);
+Mcu.InitFunction(configuration abstract type);
+Ocu.InitFunction(configuration abstract type);
+Os.InitFunction(configuration abstract type);
+Port.InitFunction(configuration abstract type);
+Pwm.InitFunction(configuration abstract type);
+SchM.InitFunction(configuration abstract type);
+Spi.InitFunction(configuration abstract type);
+Wdg.InitFunction(configuration abstract type);
+*/
 }
 
 static void DriverInitOne(void){
+}
+
+static void OnGoOffOne(void){
+}
+
+static void OnGoOffTwo(void){
+}
+
+static void Reset(void){
+}
+
+static void SwitchOff(void){
 }
 
 void class_Swc_EcuM::StartPreOs(void){
@@ -29,5 +72,56 @@ void class_Swc_EcuM::StartPreOs(void){
 }
 
 void class_Swc_EcuM::StartPostOs(void){
+   SchM.Start();
+   BswM.InitFunction(/*TBD: configuration*/);
+   SchM.InitFunction(/*TBD: configuration*/);
+   SchM.StartTiming();
 }
+
+void class_Swc_EcuM::OffPreOs(void){
+   OnGoOffOne();
+   BswM.DeInitFunction();
+   SchM.DeInitFunction();
+
+   if(EcuM.GetPendingWakeupEvents()){
+      EcuM.SelectShutdownTarget();
+   }
+}
+
+void class_Swc_EcuM::OffPostOs(void){
+   OnGoOffTwo();
+   Reset();
+   SwitchOff();
+}
+
+void class_Swc_EcuM::EnableWakeupSources(void){
+}
+
+void class_Swc_EcuM::GenerateRamHash(void){
+}
+
+void class_Swc_EcuM::CheckRamHash(void){
+}
+
+void class_Swc_EcuM::ErrorHook(void){
+}
+
+// Go to sleep sequence
+// BswM.CurrentWakeupSources();
+// Swc_EcuM.EnableWakeupSources();
+// Os.GetResource();
+
+// Go to halt sequence
+// Os.DisableAllInterrupts();
+// Swc_EcuE.GenerateRamHash();
+// Mcu.SetMode(HALT);
+
+// Go to poll sequence
+// ...
+
+// Go to wake-up restart sequence
+// ...
+
+// Go to wake-up validation sequence
+// ...
 
