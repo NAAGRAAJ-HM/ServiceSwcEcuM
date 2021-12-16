f808abd1a8640fb2e206c028971290522933b42b
root <nagaraja.hm.0001@gmail.com>
2021-12-16 10:41:28 +0100

    Update: submodule Swc_EcuM

--- LATEST/src/Swc_EcuM.cpp
+++ LATEST/src/Swc_EcuM.cpp
@@ -4,44 +4,59 @@
 #include "SchM.h"
 #include "BswM.h"
 
+ #include "Det.h"
+ #include "Dem.h"
+ #include "Fls.h"
+ #include "NvM.h"
+ #include "Mcu.h"
+ #include "Port.h"
+ #include "Gpt.h"
+ #include "Wdg.h"
+ #include "WdgM.h"
+ #include "Adc.h"
+ #include "Icu.h"
+ #include "Ocu.h"
+
 class_Swc_EcuM Swc_EcuM;
 
 static void SetProgrammableInterrupts(void){
 }
 
 static void DriverInitZero(void){
+   Det.InitFunction(/*configuration abstract type*/);
+   Dem.InitFunction(/*configuration abstract type*/);
+   Fls.InitFunction(/*configuration abstract type*/);
+   NvM.InitFunction(/*configuration abstract type*/);
+}
+
+static void DriverInitOne(void){
+   Mcu.InitFunction(/*configuration abstract type*/);
+   Port.InitFunction(/*configuration abstract type*/);
+   Gpt.InitFunction(/*configuration abstract type*/);
+   Wdg.InitFunction(/*configuration abstract type*/);
+   WdgM.InitFunction(/*configuration abstract type*/);
+   Adc.InitFunction(/*configuration abstract type*/);
+   Icu.InitFunction(/*configuration abstract type*/);
+   Ocu.InitFunction(/*configuration abstract type*/);
 /*
-Adc.InitFunction(configuration abstract type);
 BswM.InitFunction(configuration abstract type);
 Can.InitFunction(configuration abstract type);
 CanTrcv.InitFunction(configuration abstract type);
 Com.InitFunction(configuration abstract type);
 Nm.InitFunction(configuration abstract type);
-Dem.InitFunction(configuration abstract type);
-Det.InitFunction(configuration abstract type);
 Eth.InitFunction(configuration abstract type);
 EthSwt.InitFunction(configuration abstract type);
 EthTrcv.InitFunction(configuration abstract type);
-Fls.InitFunction(configuration abstract type);
 Fr.InitFunction(configuration abstract type);
 FrTrcv.InitFunction(configuration abstract type);
-Gpt.InitFunction(configuration abstract type);
-Icu.InitFunction(configuration abstract type);
 IoAbHw.InitFunction(configuration abstract type);
 Lin.InitFunction(configuration abstract type);
 LinTrcv.InitFunction(configuration abstract type);
-Mcu.InitFunction(configuration abstract type);
-Ocu.InitFunction(configuration abstract type);
 Os.InitFunction(configuration abstract type);
-Port.InitFunction(configuration abstract type);
 Pwm.InitFunction(configuration abstract type);
 SchM.InitFunction(configuration abstract type);
 Spi.InitFunction(configuration abstract type);
-Wdg.InitFunction(configuration abstract type);
 */
-}
-
-static void DriverInitOne(void){
 }
 
 static void OnGoOffOne(void){
