73dfa32a2423689a0b36aeffa3a45d279c93a6bc
root <nagaraja.hm.0001@gmail.com>
2022-03-16 12:45:33 +0100

    Update: AUTOSAR service software component SwcServiceEcuM

--- LATEST/SwcServiceEcuM.cpp
+++ LATEST/SwcServiceEcuM.cpp
@@ -48,29 +48,55 @@ class module_SwcServiceEcuM:
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
+VAR(module_SwcServiceEcuM, SWCSERVICEECUM_VAR) SwcServiceEcuM;
+CONSTP2VAR(infEcuMClient,          SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) gptrinfEcuMClient_SwcServiceEcuM = &SwcServiceEcuM;
+CONSTP2VAR(infDcmClient,           SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) gptrinfDcmClient_SwcServiceEcuM  = &SwcServiceEcuM;
+CONSTP2VAR(infSchMClient,          SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) gptrinfSchMClient_SwcServiceEcuM = &SwcServiceEcuM;
+CONSTP2VAR(infSwcServiceEcuM_EcuM, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) gptrinfSwcServiceEcuM_EcuM       = &SwcServiceEcuM;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
-
-/******************************************************************************/
-/* EOF                                                                        */
-/******************************************************************************/
-
-
-/*****************************************************/
-/* OBJECTS                                           */
-/*****************************************************/
-VAR(module_SwcServiceEcuM, SWCSERVICEECUM_VAR) SwcServiceEcuM;
-CONSTP2VAR(infEcuMClient, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) gptrinfEcuMClient_SwcServiceEcuM = &SwcServiceEcuM;
-CONSTP2VAR(infDcmClient,  SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) gptrinfDcmClient_SwcServiceEcuM  = &SwcServiceEcuM;
-CONSTP2VAR(infSchMClient, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) gptrinfSchMClient_SwcServiceEcuM = &SwcServiceEcuM;
-infSwcServiceEcuM_EcuM* gptrinfSwcServiceEcuM_EcuM       = &SwcServiceEcuM;
-
-/*****************************************************/
-/* FUNCTIONS                                         */
-/*****************************************************/
 static FUNC(void, SWCSERVICEECUM_CODE) SetProgrammableInterrupts(void){
+}
+
+static FUNC(void, SWCSERVICEECUM_CODE) DriverInitX(
+      const CONSTP2VAR(infEcuMClient, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) *laptrinfEcuMClient_DriverInitList
+   ,  uint8 luint8ListSize 
+){
+   P2VAR(infEcuMClient, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) lptrinfEcuMClient_Indexed;
+   for(
+         uint8 luint8IndexEcuMClient = 0
+      ;        luint8IndexEcuMClient < luint8ListSize 
+      ;        luint8IndexEcuMClient ++
+   ){
+      lptrinfEcuMClient_Indexed = (P2VAR(infEcuMClient, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST))laptrinfEcuMClient_DriverInitList[luint8IndexEcuMClient];
+      lptrinfEcuMClient_Indexed->InitFunction(
+         /*configuration abstract type*/
+      );
+   }
+}
+
+#include "infDet_EcuM.h"
+#include "infDem_EcuM.h"
+#include "infFls_EcuM.h"
+#include "infNvM_EcuM.h"
+
+static const CONSTP2VAR(infEcuMClient, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) laptrinfEcuMClient_DriverInitZero[] = {
+      gptrinfEcuMClient_Det
+   ,  gptrinfEcuMClient_Dem
+   ,  gptrinfEcuMClient_Fls
+   ,  gptrinfEcuMClient_NvM
+};
+
+static FUNC(void, SWCSERVICEECUM_CODE) DriverInitZero(void){
+   DriverInitX(
+         laptrinfEcuMClient_DriverInitZero
+      ,  (
+               sizeof(laptrinfEcuMClient_DriverInitZero)
+            /  sizeof(CONSTP2VAR(infEcuMClient, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST))
+         )
+   );
 }
 
 #include "infCanIf_EcuM.h"
@@ -89,7 +115,6 @@ static FUNC(void, SWCSERVICEECUM_CODE) SetProgrammableInterrupts(void){
 #include "infDio_EcuM.h"
 #include "infEep_EcuM.h"
 #include "infEth_EcuM.h"
-#include "infFls_EcuM.h"
 #include "infFr_EcuM.h"
 #include "infGpt_EcuM.h"
 #include "infIcu_EcuM.h"
@@ -105,12 +130,9 @@ static FUNC(void, SWCSERVICEECUM_CODE) SetProgrammableInterrupts(void){
 #include "infComM_EcuM.h"
 #include "infCsm_EcuM.h"
 #include "infDcm_EcuM.h"
-#include "infDem_EcuM.h"
-#include "infDet_EcuM.h"
 #include "infEcuM_SwcServiceEcuM.h"
 #include "infFiM_EcuM.h"
 #include "infNm_EcuM.h"
-#include "infNvM_EcuM.h"
 #include "infOs_EcuM.h"
 #include "infPduR_EcuM.h"
 #include "infSchM_EcuM.h"
@@ -124,63 +146,66 @@ static FUNC(void, SWCSERVICEECUM_CODE) SetProgrammableInterrupts(void){
 #include "infSwcServiceEcuM_EcuM.h"
 #include "infSwcServiceOs_EcuM.h"
 
-static FUNC(void, SWCSERVICEECUM_CODE) DriverInitZero(void){
-   gptrinfEcuMClient_Det->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Dem->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Fls->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_NvM->InitFunction(/*configuration abstract type*/);
-}
+static const CONSTP2VAR(infEcuMClient, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST) laptrinfEcuMClient_DriverInitOne[] = {
+      gptrinfEcuMClient_CanIf
+   ,  gptrinfEcuMClient_CryIf
+   ,  gptrinfEcuMClient_Ea
+   ,  gptrinfEcuMClient_EthIf
+   ,  gptrinfEcuMClient_Fee
+   ,  gptrinfEcuMClient_FrIf
+   ,  gptrinfEcuMClient_LinIf
+   ,  gptrinfEcuMClient_LinTp
+   ,  gptrinfEcuMClient_MemIf
+   ,  gptrinfEcuMClient_WdgIf
+   ,  gptrinfEcuMClient_Adc
+   ,  gptrinfEcuMClient_Can
+   ,  gptrinfEcuMClient_Cry
+   ,  gptrinfEcuMClient_Dio
+   ,  gptrinfEcuMClient_Eep
+   ,  gptrinfEcuMClient_Eth
+// ,  gptrinfEcuMClient_Fls
+   ,  gptrinfEcuMClient_Fr
+   ,  gptrinfEcuMClient_Gpt
+   ,  gptrinfEcuMClient_Icu
+   ,  gptrinfEcuMClient_Lin
+   ,  gptrinfEcuMClient_Mcu
+   ,  gptrinfEcuMClient_Ocu
+   ,  gptrinfEcuMClient_Port
+   ,  gptrinfEcuMClient_Pwm
+   ,  gptrinfEcuMClient_Spi
+   ,  gptrinfEcuMClient_Wdg
+// ,  gptrinfEcuMClient_BswM
+   ,  gptrinfEcuMClient_Com
+   ,  gptrinfEcuMClient_ComM
+   ,  gptrinfEcuMClient_Csm
+   ,  gptrinfEcuMClient_Dcm
+// ,  gptrinfEcuMClient_Dem
+// ,  gptrinfEcuMClient_Det
+   ,  gptrinfEcuMClient_FiM
+   ,  gptrinfEcuMClient_Nm
+// ,  gptrinfEcuMClient_NvM
+// ,  gptrinfEcuMClient_Os
+   ,  gptrinfEcuMClient_PduR
+// ,  gptrinfEcuMClient_SchM
+   ,  gptrinfEcuMClient_SecOC
+   ,  gptrinfEcuMClient_SokFm
+// ,  gptrinfEcuMClient_StartUp
+   ,  gptrinfEcuMClient_StbM
+   ,  gptrinfEcuMClient_Vkms
+   ,  gptrinfEcuMClient_WdgM
+   ,  gptrinfEcuMClient_Rte
+   ,  gptrinfEcuMClient_SwcServiceEcuM
+   ,  gptrinfEcuMClient_SwcServiceOs
+};
 
 static FUNC(void, SWCSERVICEECUM_CODE) DriverInitOne(void){
-   gptrinfEcuMClient_CanIf->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_CryIf->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Ea->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_EthIf->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Fee->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_FrIf->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_LinIf->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_LinTp->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_MemIf->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_WdgIf->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Adc->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Can->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Cry->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Dio->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Eep->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Eth->InitFunction(/*configuration abstract type*/);
-// gptrinfEcuMClient_Fls->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Fr->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Gpt->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Icu->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Lin->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Mcu->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Ocu->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Port->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Pwm->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Spi->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Wdg->InitFunction(/*configuration abstract type*/);
-// gptrinfEcuMClient_BswM->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Com->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_ComM->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Csm->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Dcm->InitFunction(/*configuration abstract type*/);
-// gptrinfEcuMClient_Dem->InitFunction(/*configuration abstract type*/);
-// gptrinfEcuMClient_Det->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_FiM->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Nm->InitFunction(/*configuration abstract type*/);
-// gptrinfEcuMClient_NvM->InitFunction(/*configuration abstract type*/);
-// gptrinfEcuMClient_Os->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_PduR->InitFunction(/*configuration abstract type*/);
-// gptrinfEcuMClient_SchM->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_SecOC->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_SokFm->InitFunction(/*configuration abstract type*/);
-// gptrinfEcuMClient_StartUp->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_StbM->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Vkms->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_WdgM->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_Rte->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_SwcServiceEcuM->InitFunction(/*configuration abstract type*/);
-   gptrinfEcuMClient_SwcServiceOs->InitFunction(/*configuration abstract type*/);
+   DriverInitX(
+         laptrinfEcuMClient_DriverInitOne
+      ,  (
+               sizeof(laptrinfEcuMClient_DriverInitOne)
+            /  sizeof(CONSTP2VAR(infEcuMClient, SWCSERVICEECUM_VAR, SWCSERVICEECUM_CONST))
+         )
+   );
 }
 
 static FUNC(void, SWCSERVICEECUM_CODE) OnGoOffOne(void){
@@ -278,7 +303,7 @@ FUNC(void, SWCSERVICEECUM_CODE) class_SwcServiceEcuM_Unused::ErrorHook(void){
 // Go to wake-up validation sequence
 // ...
 
-/*****************************************************/
-/* EOF                                               */
-/*****************************************************/
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
 
