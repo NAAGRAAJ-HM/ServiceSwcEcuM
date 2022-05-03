397afe47cdaef62446720a420266b3a801e6f6a5
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-03 10:10:06 +0000

    Update: Service software component for EcuM

--- LATEST/SwcServiceEcuM.cpp
+++ LATEST/SwcServiceEcuM.cpp
@@ -319,79 +319,79 @@ FUNC(void, SWCSERVICEECUM_CODE) module_SwcServiceEcuM::InitFunction(
    CONSTP2CONST(CfgModule_TypeAbstract, SWCSERVICEECUM_CONFIG_DATA, SWCSERVICEECUM_APPL_CONST) lptrCfgModule
 ){
 #if(STD_ON == SwcServiceEcuM_InitCheck)
-   if(E_OK == IsInitDone){
-#if(STD_ON == SwcServiceEcuM_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK != IsInitDone){
 #endif
-   }
-   else{
-#endif
-      if(NULL_PTR == lptrCfgModule){
-#if(STD_ON == SwcServiceEcuM_DevErrorDetect)
-         Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-         );
-#endif
-      }
-      else{
+      if(NULL_PTR != lptrCfgModule){
          if(STD_LOW){
-// check lptrCfgModule for memory faults
+            // check lptrCfgModule for memory faults
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfgCanIf as back-up configuration
+            // use PBcfgCanIf as back-up configuration
 //            lptrCfg = PBcfgSwcServiceEcuM;
          }
       }
-      IsInitDone = E_OK;
+      else{
+#if(STD_ON == SwcServiceEcuM_DevErrorDetect)
+         Det_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
 #if(STD_ON == SwcServiceEcuM_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == SwcServiceEcuM_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
 
 FUNC(void, SWCSERVICEECUM_CODE) module_SwcServiceEcuM::DeInitFunction(void){
 #if(STD_ON == SwcServiceEcuM_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == SwcServiceEcuM_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK == IsInitDone){
 #endif
+#if(STD_ON == SwcServiceEcuM_InitCheck)
+      IsInitDone = E_NOT_OK;
    }
    else{
+#if(STD_ON == SwcServiceEcuM_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
 #endif
-      IsInitDone = E_NOT_OK;
-#if(STD_ON == SwcServiceEcuM_InitCheck)
    }
 #endif
 }
 
 FUNC(void, SWCSERVICEECUM_CODE) module_SwcServiceEcuM::MainFunction(void){
 #if(STD_ON == SwcServiceEcuM_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == SwcServiceEcuM_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
-#endif
-   }
-   else{
+   if(E_OK == IsInitDone){
 #endif
 #if(STD_ON == SwcServiceEcuM_InitCheck)
+   }
+   else{
+#if(STD_ON == SwcServiceEcuM_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
