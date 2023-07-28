30d6c7f4d9049ae76819e1cf87ee454e59942684
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-07-28 08:06:40 +0200

    Initial commit

--- pRealEcu_R2023/SwcServiceEcuM.c
+++ pRealEcu_R2023/SwcServiceEcuM.c
@@ -65,14 +65,6 @@ FUNC(void, SWCSERVICEECUM_CODE) infSwcServiceEcuMSwcServiceStartUp_InitFunction(
    ){
 #endif
       SwcServiceEcuM_ePhase = SwcServiceEcuM_ePhaseStartOsPre;
-/*
-#if(ECUM_INTERRUPTS_ENABLE == TRUE)
-      EcuM_AL_SetProgrammableInterrupts();
-#endif
-      EcuM_AL_DriverInitZero();
-      EcuM_Rb_adrConfigPtr_st = EcuM_DeterminePbConfiguration();
-      EcuM_Prv_PBPointerConsistencyCheck();
-*/
 #if(ECUM_CFG_MULTICORE_ENABLED       == STD_ON)
 #if(ECUM_CFG_CCAS_INIT               == STD_OFF)
 #if(ECUM_CFG_SLAVECORE_EARLIER_START == STD_ON)
@@ -81,34 +73,15 @@ FUNC(void, SWCSERVICEECUM_CODE) infSwcServiceEcuMSwcServiceStartUp_InitFunction(
 #endif
    }
 #endif
-//   EcuM_AL_DriverInitOne(EcuM_Rb_adrConfigPtr_st);
 #if(ECUM_CFG_MULTICORE_ENABLED == STD_ON)
    if(
          ECUM_CFG_STARTUP_CORE
       == GetCoreID()
    ){
 #endif
-/*
-      EcuM_Prv_SetDefinedMcuWakeupSource();
-      EcuM_Prv_dataSelectedShutdownTarget_st.ShutdownTarget = EcuM_Rb_adrConfigPtr_st->DefaultShutdownTarget.ShutdownTarget;
-      EcuM_Prv_dataSelectedShutdownTarget_st.mode           = EcuM_Rb_adrConfigPtr_st->DefaultShutdownTarget.mode;
-      EcuM_Prv_dataSelectedShutdownTarget_st.ShutdownCause  = EcuM_Rb_adrConfigPtr_st->DefaultShutdownTarget.ShutdownCause;
-*/
 #if(ECUM_STARTUP_DURATION == TRUE)
       EcuM_Prv_dataSelectedShutdownTarget_st.TimeMeasured   = EcuM_Rb_adrConfigPtr_st->DefaultShutdownTarget.TimeMeasured;
 #endif
-/*
-      if(
-            ECUM_NO_WKUP_SRC
-         == (
-                  EcuM_Prv_dataPendingWakeupEvents_u32
-               |  EcuM_Prv_dataValidatedWakeupEvents_u32
-            )
-      ){
-         EcuM_Prv_dataValidatedWakeupEvents_u32 |= EcuMConf_EcuMWakeupSource_ECUM_WKSOURCE_RESET;
-         EcuM_Prv_dataValWkpEventsInd_u32       |= EcuMConf_EcuMWakeupSource_ECUM_WKSOURCE_RESET;
-      }
-*/
 #if(ECUM_CFG_MULTICORE_ENABLED == STD_ON)
 #if(ECUM_CFG_CCAS_INIT == STD_OFF)
 #if(ECUM_CFG_SLAVECORE_EARLIER_START == STD_OFF)
@@ -116,13 +89,10 @@ FUNC(void, SWCSERVICEECUM_CODE) infSwcServiceEcuMSwcServiceStartUp_InitFunction(
 #endif
 #endif
 #endif
-//      EcuM_Prv_flgIsModuleInitialised_b  = TRUE;
 #if(ECUM_CFG_MULTICORE_ENABLED == STD_ON)
    }
 #endif
-//   EcuM_SwitchOsAppMode();
 #if(ECUM_CFG_CCAS_INIT == STD_OFF)
-//   EcuM_Prv_StartOS();
 #endif
 }
 
